a4bc930cd7cbd0d993fc55a74225a2c3f872e766
RQuicho <raymondquicho@gmail.com>
2023-08-08 12:05:30 -0700

    Revert "updating config file to use ElephantSQL database url."
    This reverts commit 1885620fbf1ef25539026d57c6336d8a35627c16.
    
    'updating config file to use ElephantSQL database url.'

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-# SQLALCHEMY_DATABASE_URI = 'postgresql:///videogame_library'
-SQLALCHEMY_DATABASE_URI = 'postgresql://dejnmedw:ahoxQMHDIj37rHvlouEfKBJdsTtjqFyY@bubble.db.elephantsql.com/dejnmedw'
+SQLALCHEMY_DATABASE_URI = 'postgresql:///videogame_library'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 SECRET_KEY = "Keep it secret, keep it safe.."
