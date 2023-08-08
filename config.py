f8dd9b1531079b123ce5359881460332e1abc076
RQuicho <raymondquicho@gmail.com>
2023-08-08 12:16:49 -0700

    updating config file to use Environment Variable on Render for database url.

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql:///videogame_library'
+# SQLALCHEMY_DATABASE_URI = 'postgresql:///videogame_library'
+import os
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 SECRET_KEY = "Keep it secret, keep it safe.."
